db36bd2f26b7bf68f7567083140f0c185cd2f664
suhas12996 <ks.suhas12996@gmail.com>
2022-09-10 16:36:52 +0530

    second commit

---
+++ src/PractisePrograms/GitDemo.java
@@ -0,0 +1,12 @@
+package PractisePrograms;
+
+import org.apache.xpath.operations.String;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
